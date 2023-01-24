87da28d7bec6d25426d0b23593a10513336a6169
marikafikys <marikaafikys@gmail.com>
2023-01-24 20:57:00 +0400

    feat: create function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let val = str.length;
+  if (str.search(/[\[\]\{\}\(\)\|]/) == -1) {
+		return true;
+	}
+  if (str[0] == ")" || str[0] == "]" || str[0] == "}" || str[str.length-1] == "(" || str[str.length-1] == "[" || str[str.length-1] == "{" || str.length % 2 !== 0) {
+		return false;
+	} else {
+      for (let i = 0; i <= val/2; i++) {
+        str = str.replace("()", "");
+        str = str.replace("[]", "");
+        str = str.replace("{}", "");
+        str = str.replace("||", "");
+      }
+  }
+  if (str == "" || str == "()" || str == "[]" || str == "{}" || str == "||") {return true} else {return false};
 }
+
+// function check(str, bracketsConfig) {
+//   let val = str.length;
+//   if (str.search(/[\[\]\{\}\(\)\|]/) == -1) {
+// 		return true;
+// 	}
+//   if (str[0] == ")" || str[0] == "]" || str[0] == "}" || str[str.length-1] == "(" || str[str.length-1] == "[" || str[str.length-1] == "{" || str.length % 2 !== 0) {
+// 		return false;
+// 	} else {
+//       for (let i = 0; i <= val/2; i++) {
+//         str = str.replace("()", "");
+//         str = str.replace("[]", "");
+//         str = str.replace("{}", "");
+//         str = str.replace("||", "");
+//         console.log(i, str);
+//       }
+//   }
+//   if (str == "" || str == "()" || str == "[]" || str == "{}" || str == "||") {return true} else {return false};
+// }
+
+// console.log(check('([[[[(({{{}}}(([](((((((())))||||||))))[[{{|{{}}|}}[[[[]]]]{{{{{}}}}}]]))))]]]])((([[[[(({{{}}}(([](((((((())))||||||))))[[{{|{{}}|}}[[[[]]]]{{{{{}}}}}]]))))]]]])))'));
+
+
