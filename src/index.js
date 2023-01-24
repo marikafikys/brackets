e623b4da780e0580ed4b549fc1720ca02738971d
marikafikys <marikaafikys@gmail.com>
2023-01-24 22:04:53 +0400

    fix: update function

--- src/index.js
+++ src/index.js
@@ -1,40 +1,35 @@
 module.exports = function check(str, bracketsConfig) {
-  let val = str.length;
-  if (str.search(/[\[\]\{\}\(\)\|]/) == -1) {
-		return true;
-	}
-  if (str[0] == ")" || str[0] == "]" || str[0] == "}" || str[str.length-1] == "(" || str[str.length-1] == "[" || str[str.length-1] == "{" || str.length % 2 !== 0) {
-		return false;
-	} else {
-      for (let i = 0; i <= val/2; i++) {
-        str = str.replace("()", "");
-        str = str.replace("[]", "");
-        str = str.replace("{}", "");
-        str = str.replace("||", "");
-      }
+  let stack = [];
+  
+  let form = {};
+
+  let type = {};
+
+  for (let i = 0; i < bracketsConfig.length; i++) {
+
+    let left = bracketsConfig[i][0];
+    let right = bracketsConfig[i][1];
+    form[left] = right;
+    form[right] = left;
+    type[left] = "opened";
+    type[right] = "closed";
+
   }
-  if (str == "" || str == "()" || str == "[]" || str == "{}" || str == "||") {return true} else {return false};
+
+  for (let i = 0; i < str.length; i++) {
+    let top = stack.length <= 0 ? null : stack[stack.length - 1];
+    let curr = str[i];
+    if (type[curr] == "opened") {
+      stack.push(curr);
+    } else if (type[curr] == "closed" && curr !== form[curr] && stack.length == 0) {
+      return false;
+    } else if (type[curr] == "closed" && top == form[curr]) {
+      stack.pop();
+    } else if (type[curr] == "closed" && curr == form[curr] ){
+      stack.push(curr);
+    }
+  }
+  return stack.length == 0 ? true : false;
 }
-
-// function check(str, bracketsConfig) {
-//   let val = str.length;
-//   if (str.search(/[\[\]\{\}\(\)\|]/) == -1) {
-// 		return true;
-// 	}
-//   if (str[0] == ")" || str[0] == "]" || str[0] == "}" || str[str.length-1] == "(" || str[str.length-1] == "[" || str[str.length-1] == "{" || str.length % 2 !== 0) {
-// 		return false;
-// 	} else {
-//       for (let i = 0; i <= val/2; i++) {
-//         str = str.replace("()", "");
-//         str = str.replace("[]", "");
-//         str = str.replace("{}", "");
-//         str = str.replace("||", "");
-//         console.log(i, str);
-//       }
-//   }
-//   if (str == "" || str == "()" || str == "[]" || str == "{}" || str == "||") {return true} else {return false};
-// }
-
-// console.log(check('([[[[(({{{}}}(([](((((((())))||||||))))[[{{|{{}}|}}[[[[]]]]{{{{{}}}}}]]))))]]]])((([[[[(({{{}}}(([](((((((())))||||||))))[[{{|{{}}|}}[[[[]]]]{{{{{}}}}}]]))))]]]])))'));
 
 
--- test.js
+++ test.js
@@ -58,11 +58,11 @@ it('should check if brackets sequence is correct 12', () => {
 });
 
 it('should check if brackets sequence is correct 13', () => {
-  assert.equal(check('5555512575557777777555566667888888667661133833448441111222233333444442266666', config6), true);
+  assert.equal(check('5555512575557777777555566667888888667661133833448441111222233333444442266666', config6), false);
 });
 
 it('should check if brackets sequence is correct 14', () => {
-  assert.equal(check('8888877878887777777888888887777777887887788788887887777777788888888887788888', config6), true);
+  assert.equal(check('8888877878887777777888888887777777887887788788887887777777788888888887788888', config6), false);
 });
 
 it('should check if brackets sequence is correct 15', () => {
